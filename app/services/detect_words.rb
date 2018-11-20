d15236babe9aaf3f25043c877496679108b38b6c
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-20 22:13:40 +0900

    modify

--- app/services/detect_words.rb
+++ app/services/detect_words.rb
@@ -1,15 +1,10 @@
 require 'google/cloud/vision'
 
-
 class DetectWords
   def self.call(image_path)
-    key_file = "/Users/yutafujii/Downloads/miru-lw187-997a31b5b6ef.json"
-
+    # project_id = Rails.configuration.google_cloud_api[:project]
     vision = Google::Cloud::Vision.new project: "miru-lw187"
-    text  = vision.image(image_path).text
-    puts text.text
+    text = vision.image(image_path).text
+    p text.text
   end
 end
-
-url = "/Users/yutafujii/Downloads/menu.jpg"
-DetectWords.call(url)
