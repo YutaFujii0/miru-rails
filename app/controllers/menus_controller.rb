705af9b6f3c7c5f98f5266295f8a4203b1e384be
Arif Iwamoto <arif.iwamoto@gmail.com>
2018-11-20 12:13:28 +0900

    method controlelr

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,13 @@
 class MenusController < ApplicationController
   def new
+    @menu = Menu.new
   end
   def create
+    @menu = Menu.new(menu_params)
+    @menu.user = current_user
+  end
+
+  def menu_params
+    params.require(:menu).permit(:photo)
   end
 end
