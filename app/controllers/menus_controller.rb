d1958731e9ae19ce683ffcfbef2566444dc87b17
Arif Iwamoto <arif.iwamoto@gmail.com>
2018-11-20 15:55:23 +0900

    testing

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -5,7 +5,6 @@ class MenusController < ApplicationController
     vision = Google::Cloud::Vision.new
     file_url = "app/assets/images/curry.jpg"
     @vis = vision.image(file_url).label
-    raise
   end
   def create
     @menu = Menu.new(menu_params)
