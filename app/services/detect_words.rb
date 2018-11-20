c9eef507e6c36413f89688e9814ee1a7d1abe522
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-21 01:34:46 +0900

    Create search method

--- app/services/detect_words.rb
+++ app/services/detect_words.rb
@@ -5,6 +5,6 @@ class DetectWords
     # project_id = Rails.configuration.google_cloud_api[:project]
     vision = Google::Cloud::Vision.new project: "miru-lw187"
     text = vision.image(image_path).text
-    p text.text
+    text.text
   end
 end
