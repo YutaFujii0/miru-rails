0da2cb20b1e0d58f0f047aeb82aaa7826e8c473b
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-21 16:04:25 +0900

    Create refine method

--- app/services/refine_words.rb
+++ app/services/refine_words.rb
@@ -1,5 +1,21 @@
 class RefineWords
   def self.call(words)
-    words.split("\n").first(5)
+
+    # ascii code
+
+    # omit letters in parentheses
+    words.gsub!(/\(.*\)/, "")
+    # omit numbers
+    words.gsub!(/(\d+.?)/, "")
+    # omit non-alphabet letters(pipes, commas, slashes)
+    words.gsub!(/[\|\/\,\.\:\;\[\]]/, "")
+    puts "after 3rd action #{words}"
+    # after these actions, omit whitespaces both head and tail
+    # words.strip!
+    # call reduce method with empty? in the brock
+    words.split("\n").reject do |word|
+      word.strip!
+      word.empty?
+    end
   end
 end
