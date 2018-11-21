92d016971211b9100ad92dd47270ed0d55a94ad7
Arif Iwamoto <arif.iwamoto@gmail.com>
2018-11-21 16:04:55 +0900

    still failing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,16 @@
 //= require rails-ujs
 //= require_tree .
+new Vue({
+  el: "#app",
+  data: {
+    clicked: false,
+    index: 0
+  },
+  methods: {
+    logEvent(e) {
+      this.clicked = !this.clicked
+      index++;
+    }
+  }
+
+})
