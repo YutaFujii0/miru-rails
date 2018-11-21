9a56f3fbfb478c1dab68135a3f312ad9570d6255
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-21 14:53:11 +0900

    Add images column to food

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_031212) do
+ActiveRecord::Schema.define(version: 2018_11_21_052946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_11_20_031212) do
     t.boolean "deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "images", array: true
   end
 
   create_table "menus", force: :cascade do |t|
