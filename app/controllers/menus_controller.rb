4c98eedb019011436d66e6fa6c8288a839d3990c
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-23 01:48:14 +0900

    refactor methods to improve readability

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -4,8 +4,14 @@ class MenusController < ApplicationController
   end
 
   def create
-    # split the job to increase performance
-    words = GoogleCloudVisionJob.perform_now(params[:menu][:photo].tempfile.path)
+    # ==========================================
+    # ***** FOP DEVELOPMENT purpose *****
+    detect_words = "-7 チキンナンバン690\nかきあげ690 |\n山田醤油\nおでん盛合せ690|みりん干し,590モモステーキ\n大根玉子竹輪なると11唐揚げ 590\nちくわぶつみれさつま揚\nおろし葱ソース\nサーモンハラス焼590-チューリップ唐揚げ-\nワカサギ天ぷら490\n芝海老かき揚げ590\nレモンバター焼き690\n:\n2本290"
+    words = RefineWords.call(detect_words)
+    # ***** FOP PRODUCTION purpose *****
+    # words = GoogleCloudVisionJob.perform_now(params[:menu][:photo].tempfile.path)
+    # ==========================================
+
     # make new instance without any params on purpose
     @menu = Menu.new
     @menu.user = current_user
