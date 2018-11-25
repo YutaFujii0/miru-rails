9d8470183a4b2d6378682f0cbe5110acd49a1bbb
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-25 17:00:23 +0900

    create js to change body content

--- app/javascript/packs/results/order_selector.js
+++ app/javascript/packs/results/order_selector.js
@@ -1,16 +1,31 @@
-const cards = document.querySelectorAll(".card-order-list img");
+const images = document.querySelectorAll(".card-order-list img");
+const bodyContents = document.querySelectorAll(".card-result-body");
 
 const changeActiveSelector = (event) => {
   const targetCard = event.target.parentNode;
-  cards.forEach((card) => {
-    card.parentNode.classList.remove("active");
+  images.forEach((image) => {
+    image.parentNode.classList.remove("active");
   });
   targetCard.classList.add("active");
 }
 
+const changeBodyContent = (event) => {
+  const index = event.target.classList[1];
+  console.log(`.card-result-body .${index}`);
+  const targetBodyContent = document.querySelector(`.card-result-body.${index}`);
+  console.log(targetBodyContent);
+  bodyContents.forEach((content) => {
+    content.classList.add("hidden");
+  })
+  targetBodyContent.classList.remove("hidden");
+}
+
 function selectItem() {
-  cards.forEach((card) => {
-    card.addEventListener("click", changeActiveSelector);
+  images.forEach((image) => {
+    image.addEventListener("click", (event) => {
+      changeActiveSelector(event);
+      changeBodyContent(event);
+    });
   });
 }
 
