402d726d3a02482e57f2420db44c132f192e793d
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-26 14:49:11 +0900

    change searching method 1)retrieve language from menu pic 2) save image paths into food table

--- app/services/detect_words.rb
+++ app/services/detect_words.rb
@@ -4,8 +4,6 @@ class DetectWords
   def self.call(image_path)
     vision = Google::Cloud::Vision.new project: ENV["GOOGLE_CLOUD_PROJECT"]
     image = vision.image(image_path)
-    p image.context.languages
-
-    p image.text.text
+    { language: image.text.locale, text: image.text.text }
   end
 end
