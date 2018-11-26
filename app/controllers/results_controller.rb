7881a91fefcfdf56fea85368e85a7136aef457d5
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-27 02:57:38 +0900

    Add explanation

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,15 +1,13 @@
 class ResultsController < ApplicationController
   def index
-    @menu = Menu.where(user_id: current_user.id)
     # TODO: return all results with image paths
     # set the instances
-    @results = Menu.find(params[:menu_id]).results
     # search images for each food
-    @results_with_data = search_image_for_each_food(@results)
+    @results = Menu.find(params[:menu_id]).results
+    search_image_for_each_food(@results)
   end
 
   def order
-    # @menu = Menu.where(user_id: current_user.id)
     @orders = Result.where("results.order > ?", 0).where(menu_id: params[:menu_id].to_i)
 
     url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=udon"
@@ -17,8 +15,6 @@ class ResultsController < ApplicationController
     food_wiki = JSON.parse(open(url).read)
     @food_title = food_wiki["query"]["pages"].values[0]["title"]
     @food_summary = food_wiki["query"]["pages"].values[0]["extract"]
-
-    # search_image_for_each_food_order(@orders)
   end
 
   def update
@@ -34,15 +30,18 @@ class ResultsController < ApplicationController
   private
 
   def search_image_for_each_food(results)
-    # boost threads to imcrease performance
+    # boost threads to increase performance
     pool = Concurrent::FixedThreadPool.new(10)
     completed = []
 
-    translation_of_meal = Language.find_by(code: results.first.lang).meal_is
+    translation_of_meal = Language.find_by(code: results.first.lang).meal_is # -> REFERENCE 1 (refer to the bottom)
     results.each do |result|
       pool.post do
         # ==========================================
-        result.food.images = [Food::SAMPLE_IMAGES.sample] if result.food.images.nil?
+        if result.food.images.nil?
+          result.food.images = [Food::SAMPLE_IMAGES.sample]
+          result.food.save!
+        end
         # ------------------------------------------
         # if result.food.images.nil?
         #   keyword = "#{result.food.name}+#{translation_of_meal}"
@@ -55,10 +54,21 @@ class ResultsController < ApplicationController
         completed << 1
       end
     end
-
     # temporary measure: wait_for_termination does not work well
     sleep(0.1) unless completed.count == results.count
     pool.shutdown
     pool.wait_for_termination
   end
 end
+
+# REFERENCES
+# 1. Here, we get the string of 'meal' translated to the language of the menu.
+#     This variable is used for creating keyword which we pass to the API.
+#     The reason for this is to improve the relevance of the result images.
+#     For example, Japanese menu has lot of fishes which really is 'sashimi',
+#     but if you put those words API returns the fish itself not the meal.
+#     There are other possibilities as an additional keyword to search with:
+#       - food(english)
+#       - meal(english)
+#       - 'food'(translated to that lang)
+#     However we feel 'meal'(translated) is the best performer.
