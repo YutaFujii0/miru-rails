146e2f21727894cdf44f335714c0fb9ae092edd0
Raymond Cheong <r_a_y_c@hotmail.com>
2018-11-27 09:54:49 +0900

    Fav show page

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,7 +3,6 @@ class FoodsController < ApplicationController
     # @result = Result.find(params[:id])
     # @food = Food.find(@result.food.id)
     @food = Food.find(params[:id])
-    @result = Result.find(params[:result_id])
     @favourite = current_user.favourites.find_by(food: @food)
     @food_images = SearchImages.call(@food.name)
     @fav = Favourite.where(user_id: current_user) #footer favourite number
