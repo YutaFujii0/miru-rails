c29024092d47bd31339aed9941df24093316a31c
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-27 09:56:10 +0900

    fixed the naming

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,7 +4,7 @@ class FoodsController < ApplicationController
     # @food = Food.find(@result.food.id)
     @food = Food.find(params[:id])
     @favourite = current_user.favourites.find_by(food: @food)
-    @food_images = SearchImages.call(@food.name)
+    @food_images = SearchImagesAndPopularity.call(@food.name)
     @fav = Favourite.where(user_id: current_user) #footer favourite number
   end
 end
