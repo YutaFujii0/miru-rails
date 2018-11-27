9e61936379c9cd3f27cb6e3a14f220a424e3a54c
Yuta Fujii <y.f.mar2@gmail.com>
2018-11-27 23:27:31 +0900

    set safety operator in google cloud vision api

--- app/services/detect_words.rb
+++ app/services/detect_words.rb
@@ -4,7 +4,7 @@ class DetectWords
   def self.call(image_path)
     vision = Google::Cloud::Vision.new project: ENV["GOOGLE_CLOUD_PROJECT"]
     image = vision.image(image_path)
-    { language: image.text.locale, text: image.text.text }
+    { language: image&.text&.locale, text: image&.text&.text } # -> REFERENCE 1 (refer to the bottom)
   end
 end
 
@@ -12,3 +12,11 @@ end
 # - words will be used to show the result
 # - language will be used to improve the relevance of the images of that meal
 # See app/services/search_images.rb for more detail.
+
+# REFERENCES
+# 1. We set safe navigation operator &.
+#     when image has no letters, image.text.locale/image.text.text will cause
+#     undefined method *** to the NilClass error.
+#     To address this problem, we set safety operator.
+#
+#     http://mitrev.net/ruby/2015/11/13/the-operator-in-ruby/
